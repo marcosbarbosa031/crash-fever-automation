873f1d69f4b60116aa450c8afb71b95c9fd8fd8e
Marcos Barbosa <marcosbarbosa031@gmail.com>
2021-07-15 02:02:29 -0300

    Finding auto button on game

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import mss
 import cv2
+import keyboard
+import pyautogui
 import numpy as np
+from enum import Enum
+from time import time
 
 sct = mss.mss()
 
 # Constants
+pyautogui.PAUSE = 0
+
 # Threshold
 BLUESTACKS_THRESHOLD = .90
+AUTO_THRESHOLD = .80
+
 # Box Drawing
 BOX_COLOR = (0, 255, 0)
 BOX_BORDER_WIDTH = 2
 
-# Target Imgs
-bluestacks_logo_img = cv2.imread('assets/bluestacks.png')
-bluestacks_size_img = cv2.imread('assets/bluestacks_size.png')
+# Game state
+class GAME_STATES(Enum):
+    GAME_NOT_STARTED = 0
+    GAME_AUTO_OFF = 1
+    GAME_AUTO_ON = 2
+    GAME_END_MESSAGE = 3
+    GAME_END_BONUS = 4
+    GAME_QUEST_CLEAR_POPUP = 5
+    GAME_QUEST_CLEAR = 6
+
+game_state = GAME_STATES.GAME_NOT_STARTED
+
+# Drawing
+RECT_COLOR = (0, 255, 0)
+RECT_BORDER_WIDTH = 2
+
+# Target Images
+auto_btn_img = cv2.imread('assets/auto_btn.png')
+ok_btn_img = cv2.imread('assets/ok_btn.png')
+continue_btn_img = cv2.imread('assets/continue_btn.png')
+ok_btn_img = cv2.imread('assets/ok_btn.png')
+retry_btn_img = cv2.imread('assets/retry_btn.png')
 
 # Get monitor screen
 def get_monitor_scr():
@@ -22,7 +49,8 @@ def get_monitor_scr():
     @returns Image with the screenshot.
     """
     img = np.array(sct.grab(sct.monitors[1]))
-    return img[:,:,:3]
+    img = img[:,:,:3]
+    return img.copy()
 
 # Match and image with another
 def match_image(compare_img, target_img):
@@ -75,8 +103,7 @@ def open_image(title, image):
     @param image The image.
     """
     cv2.imshow(title, image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    cv2.waitKey(1)
 
 # Check if the BlueStacks window is maximized
 def is_bluestacks_maximized(bluestacks_xloc):
@@ -89,8 +116,8 @@ def is_bluestacks_maximized(bluestacks_xloc):
     return bluestacks_xloc < 10
 
 # Draw rectangle in image
-def draw_rect(image, start_vertex, end_vertex, rect_border_width):
-    """Drawn an rectangle on the given image on the given location.
+def draw_rect(image, start_vertex, end_vertex, rect_color, rect_border_width):
+    """Drawn a rectangle on the image on a specific location.
 
     @param image The image.
     @param start_vertex Initial vertex of the rectangle.
@@ -98,36 +125,67 @@ def draw_rect(image, start_vertex, end_vertex, rect_border_width):
     @param color The color in (0, 255, 255) format.
     @param rect_border_width Size of the rect border in pixels.
     """
-    cv2.rectangle(image, start_vertex, end_vertex, rect_border_width)
+    cv2.rectangle(image, start_vertex, end_vertex, rect_color, rect_border_width)
+
+# Print FPS
+def show_fps(fps_time):
+    """Show FPS on console
+
+    @param fps_time Time use to calc FPS.
+
+    @returns Updated time.
+    """
+    try:
+        print('FPS: %.0f' % (1 / (time() - fps_time)))
+    except:
+        None
+    return time()
+
+# Go to next game state
+def next_state():
+    """Update game state to next state
+
+    @returns Updated game state.
+    """
+    return GAME_STATES(game_state.value + 1)
+
+# Click the image found on scree
+def click_img_on_screen(img, img_loc):
+    """Click on the image found on screen
+
+    @param img The image to click.
+    @param img_loc Tuple with image x and y location.
+    """
+    w, h = get_img_dimension(img)
+    draw_rect(monitor_img, img_loc, (img_loc[0] + w, img_loc[1] + h), RECT_COLOR, RECT_BORDER_WIDTH)
+
+    click_x = max_loc[0] + (w/2)
+    click_y = max_loc[1] + (h/2)
+
+    pyautogui.click(click_x, click_y)
 
 
 # Script Start
-monitor_img = get_monitor_scr()
+fps_time = time()
 
-# Match bluestacks windows
-bluestacks_min_val, bluestacks_max_val, bluestacks_min_loc, bluestacks_max_loc = match_image(
-    monitor_img, bluestacks_logo_img)
+while True:
+    print(game_state.name)
+    monitor_img = get_monitor_scr()
 
-print('Accuracy of BlueStacks window: %.2f%%' % bluestacks_max_val)
+    # Verify game state
+    if game_state == GAME_STATES.GAME_NOT_STARTED:
+        min_val, max_val, min_loc, max_loc = match_image(monitor_img, auto_btn_img)
 
-# Get bluestacks window dimensions
-bluestacks_w, bluestacks_h = get_img_dimension(bluestacks_size_img)
+        print("Accuracy: %.2f%%" % max_val)
 
-if is_accuracy_above_threshold(bluestacks_max_val, BLUESTACKS_THRESHOLD):
-    if is_bluestacks_maximized(bluestacks_max_loc[0]):
-        # Get entire monitor image
-        image_to_match = monitor_img
-    else:
-        # Get dimesion of bluestacks window
-        dimensions_bluestacks = {
-            'left': bluestacks_max_loc[0],
-            'top': bluestacks_max_loc[1],
-            'width': bluestacks_w,
-            'height': bluestacks_h
-        }
+        if is_accuracy_above_threshold(max_val, AUTO_THRESHOLD):
+            click_img_on_screen(auto_btn_img, max_loc)
+            game_state = next_state()
 
-        image_to_match = get_monitor_segment_img(dimensions_bluestacks)
 
-    open_image('Image to Match', image_to_match)
-else:
-    print('BlueStacks window not found. Check if the program is open, not minimized and in your primary monitor')
+    open_image('Image to Match', monitor_img)
+
+    if keyboard.is_pressed('c'):
+        break
+
+    fps_time = show_fps(fps_time)
